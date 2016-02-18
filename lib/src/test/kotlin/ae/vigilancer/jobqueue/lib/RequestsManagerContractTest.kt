3822e4dd034f14b3a3fac881b0a40bbce808dbfd
Andrey Elizarov <vigilancer@gmail.com>
2016-02-19 01:05:22 +0300

    move, rename, etc

---
+++ lib/src/test/kotlin/ae/vigilancer/jobqueue/lib/RequestsManagerContractTest.kt
@@ -0,0 +1,25 @@
+package ae.vigilancer.jobqueue.lib
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class RequestsManagerContractTest {
+
+
+    // проверки [RequestsManager]
+    // проверка что transformer применяются
+    // проверка что трансформеры применяются в том же порядке, что и заданы в init()
+    // (выполнение контракта) сквозь RequestsManager могут безболезненно проходить null-ы
+
+
+    // проверка что все трансформеры применяются (больше 2х)   ( .compose(ts) работает )
+
+    // что задачи выполняются даже когда нет подписчиков
+
+    // что задача выполняется после того как все подписчики отписались
+
+    // что подписчики получают только результаты задач, готовых на момент подписки, а не всех с самого начала работы RequestManager-а
+
+
+}
